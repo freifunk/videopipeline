f0bddef6797d122b30386152328193f5248baac5
itsvijayreddy <tamalampudivijayakumar@gmail.com>
2022-06-25 00:48:22 +0530

    update .gitignore and script files

--- scripts/encode_h264_AAC_HQ.sh
+++ scripts/encode_h264_AAC_HQ.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 SOURCE=$1
-ORIGINAL_FILE_PATH=$2
-TARGET=$3
-TITLE=$4
-TARGET_BASE=$(basename "${ORIGINAL_FILE_PATH%.*}")
+TARGET=$2
+TITLE=$3
 
-ffmpeg -v warning -i "${SOURCE}/${TARGET_BASE}.mp4" -vf yadif \
+ffmpeg -v warning -i "${SOURCE}" -vf yadif \
   -c:v libx264 -pix_fmt yuv420p -crf:v 23 -profile:v high -level:v 4.2 \
-  -c:a libfdk_aac -b:a 128k -metadata title="'${TITLE}'" \
+  -b:a 128k -metadata title="'${TITLE}'" \
     -metadata album="<album>" \
     -metadata copyright="Licensed to the public under http://creativecommons.org/licenses/by-sa/3.0/" \
-    -f mp4 -movflags faststart -v 24 -y "${TARGET}/${TARGET_BASE}.mp4"
+    -f mp4 -movflags faststart -v 24 -y "out.mp4"
