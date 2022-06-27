9c54774851fb84273db6128ec8467cdbb88215e0
Vijay Reddy <tamalampudivijayakumar@gmail.com>
2022-06-27 23:27:54 +0530

    Added tasks to encode-h264 job

--- scripts/fix-metadata.sh
+++ scripts/fix-metadata.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 INPUTFILE=$1
-OUTPUTPATH=$2
-OUTPUTFILE=$(basename "${INPUTFILE%.*}.mp4")
 
 ffmpeg -i "${INPUTFILE}" \
   -map_metadata 0 \
   -metadata:s:a:0 LICENSE="Licensed to the public under https://creativecommons.org/licenses/by/2.0/de/ - http://media.freifunk.net" \
-  -c:a copy -v 24 -y "${OUTPUTPATH}/${OUTPUTFILE}"
+  -c:a copy -v 24 -y meta-fix.mp4
