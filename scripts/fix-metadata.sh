1fdef50f554233f15cbf6d103068aee53162bb69
Vijay Reddy <tamalampudivijayakumar@gmail.com>
2022-07-03 13:50:45 +0530

    Fixed small problem in pipeline

--- scripts/fix-metadata.sh
+++ scripts/fix-metadata.sh
@@ -5,4 +5,4 @@ FORMAT=$1
 ffmpeg -i "$video_id-processed.${FORMAT}"   \
   -map_metadata 0 \
   -metadata:s:a:0 LICENSE="Licensed to the public under https://creativecommons.org/licenses/by/2.0/de/ - http://media.freifunk.net" \
-  -c:a copy -v 24 -y "$video_id.${FORMAT}"
+  -c:a copy -v 24 -y "$video_id-fixed.${FORMAT}"
