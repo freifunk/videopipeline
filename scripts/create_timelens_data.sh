177f5b073cb9522e23eefc9e88eb7e568021ca86
Vijay <vijay@Vijays-MacBook-Air.local>
2022-09-19 15:33:09 +0530

    Updated timelens script and added timelens step to pipeline

--- scripts/create_timelens_data.sh
+++ scripts/create_timelens_data.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-SOURCE=$1
-ORIGINAL_FILE_PATH=$2
-TARGET=$3
-TARGET_BASE=$(basename "${ORIGINAL_FILE_PATH%.*}")
+FILE=$1
 
-timelens "${SOURCE}/${TARGET_BASE}.mp4" \
-  --timeline "${TARGET}/${TARGET_BASE}.timeline.jpg"\
-  --thumbnails "${TARGET}/${TARGET_BASE}.vtt"
+timelens "$FILE" \
+  --timeline "$VIDEO_ID.timeline.jpg"\
+  --thumbnails "$VIDEO_ID.vtt"
