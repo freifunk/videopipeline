af2b41c678ac37ec343851d0aefa15fb21e9aacb
0xvj <tamalampudivijayakumar@gmail.com>
2022-09-23 21:14:37 +0530

    small fix in script and pipeline

--- scripts/upload_video_to_cdn.sh
+++ scripts/upload_video_to_cdn.sh
@@ -9,4 +9,4 @@ fi
 
 rsync -a ${VIDEO_ID}-${FORMAT}.${EXT} ${CDN_SERVER_USERNAME}@${CDN_ADDRESS}:${CDN_FILES_FOLDER}/${ACRONYM}/${VIDEO_ID}/
 
-mv ${VIDEO_ID}-${FORMAT}.${EXT} video.${EXT}
+mv ${VIDEO_ID}-${FORMAT}.${EXT} video-${FORMAT}.${EXT}
