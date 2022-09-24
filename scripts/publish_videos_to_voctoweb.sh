dc3d8ba9e83d981c288b6c1630c699be530a3a57
0xvj <tamalampudivijayakumar@gmail.com>
2022-09-24 20:16:54 +0530

    updated voctoweb publish script

--- scripts/publish_videos_to_voctoweb.sh
+++ scripts/publish_videos_to_voctoweb.sh
@@ -1,64 +1,79 @@
 #!/usr/bin/env bash
 
+# VOCTOWEB_URL="http://52.66.255.140:3000"
+# API_KEY="6d72a1de-f38d-416c-bcaa-b9612de24d74"
+# ACRONYM="freifunk"
+# LANGUAGE="eng"
+# SLUG="freifunk-2022"
+# TITLE="Test title"
+# SUBTITLE="Test subtitle"
+# DESCRIPTION="Test Description"
+# PERSONS="vijay andi"
+# TAGS="test video pipeline"
+# LINK="https://github.com/freifunk/videopipeline"
+# DATE="1234"
+# RELEASE_DATE="1234"
+# VIDEO_ID="dd3456dd-e277-4947-95db-3b7c64d4216f"
+
+
+
+
 VIDEO_LENGTH=$(ffprobe -loglevel quiet -print_format default -show_format video-h264.mp4 | grep duration= | sed -e 's/duration=\([[:digit:]]*\).*/\1/g')
 VIDEO_WIDTH=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1 video-h264.mp4)
 VIDEO_HEIGHT=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 video-h264.mp4)
 MP4_SIZE=$(( $( stat -c '%s' video-h264.mp4) / 1024 / 1024 )) 
 WEBM_SIZE=$(( $( stat -c '%s' video-webm.webm) / 1024 / 1024 )) 
 
-curl -H "CONTENT-TYPE: application/json" -d '{
-   "api_key":'${API_KEY}',
-    "acronym":'${ACRONYM}',
+curl -o /dev/null -s -H  "CONTENT-TYPE: application/json" -d '{
+   "api_key":'"\"${API_KEY}\""',
+    "acronym":'"\"${ACRONYM}\""',
     "event":{
-      "guid":'${VIDEO_ID}',
-      "original_language":'${LANGUAGE}',
-      "slug":'${SLUG}',
-      "title":'${TITLE}',
-      "subtitle":'${SUBTITLE}',
-      "description":'${DESCRIPTION}',
-      "persons":'${PERSONS}',
-      "tags":'${TAGS}',
-      "link":'${LINK}',
-      "date":'${DATE}',
-      "release_date":'${RELEASE_DATE}',
-      "poster_url":'${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}_preview.jpg',
-      "thumb_url":'${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}_thumb.jpg',
-      "timeline_url":'${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}.timeline.jpg',
-      "thumbnails_url":'${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}.vtt'
+      "guid":'"\"${VIDEO_ID}\""',
+      "original_language":'"\"${LANGUAGE}\""',
+      "slug":'"\"${SLUG}\""',
+      "title":'"\"${TITLE}\""',
+      "subtitle":'"\"${SUBTITLE}\""',
+      "description":'"\"${DESCRIPTION}\""',
+      "persons":'"\"${PERSONS}\""',
+      "tags":'"\"${TAGS}\""',
+      "link":'"\"${LINK}\""',
+      "date":'"\"${DATE}\""',
+      "release_date":'"\"${RELEASE_DATE}\""',
+      "poster_url":'"\"${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}_preview.jpg\""',
+      "thumb_url":'"\"${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}_thumb.jpg\""',
+      "timeline_url":'"\"${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}.timeline.jpg\""',
+      "thumbnails_url":'"\"${ACRONYM}/${VIDEO_ID}/${VIDEO_ID}.vtt\""'
     }
   }' "${VOCTOWEB_URL}/api/events"
 
 
-curl -H "CONTENT-TYPE: application/json" -d '{
-    
-   "api_key":'${API_KEY}',
-    "guid":'${VIDEO_ID}',
+curl -o /dev/null -s -H  "CONTENT-TYPE: application/json" -d '{
+   "api_key":'\"${API_KEY}\"',
+    "guid":'\"${VIDEO_ID}\"',
     "recording":{
-      "filename":'${VIDEO_ID}-h264.mp4',
-      "folder":'${ACRONYM}/${VIDEO_ID}',
-      "mime_type":"video/mp4",
-      "language":'${LANGUAGE}',
-      "size":'${MP4_SIZE}',
-      "length":'${VIDEO_LENGTH}',
-      "width":'${VIDEO_HEIGHT}',
-      "height":'${VIDEO_WIDTH}'
+      "filename":'\"${VIDEO_ID}-h264.mp4\"',
+      "folder":'\"${ACRONYM}/${VIDEO_ID}\"',
+      "mime_type":'\"video/mp4\"',
+      "language":'\"${LANGUAGE}\"',
+      "size":'\"${MP4_SIZE}\"',
+      "length":'\"${VIDEO_LENGTH}\"',
+      "width":'\"${VIDEO_HEIGHT}\"',
+      "height":'\"${VIDEO_WIDTH}\"'
     }
   }' "${VOCTOWEB_URL}/api/recordings"
 
 
-curl -H "CONTENT-TYPE: application/json" -d '{
-    
-   "api_key":'${API_KEY}',
-    "guid":'${VIDEO_ID}',
+curl -o /dev/null -s -H  "CONTENT-TYPE: application/json" -d '{
+   "api_key":'\"${API_KEY}\"',
+    "guid":'\"${VIDEO_ID}\"',
     "recording":{
-      "filename":'${VIDEO_ID}-webm.webm',
-      "folder":'${ACRONYM}/${VIDEO_ID}',
-      "mime_type":"video/webm",
-      "language":'${LANGUAGE}',
-      "size":'${WEBM_SIZE}',
-      "length":'${VIDEO_LENGTH}',
-      "width":'${VIDEO_HEIGHT}',
-      "height":'${VIDEO_WIDTH}'
+      "filename":'\"${VIDEO_ID}-webm.webm\"',
+      "folder":'\"${ACRONYM}/${VIDEO_ID}\"',
+      "mime_type":'\"video/webm\"',
+      "language":'\"${LANGUAGE}\"',
+      "size":'\"${WEBM_SIZE}\"',
+      "length":'\"${VIDEO_LENGTH}\"',
+      "width":'\"${VIDEO_HEIGHT}\"',
+      "height":'\"${VIDEO_WIDTH}\"'
     }
   }' "${VOCTOWEB_URL}/api/recordings"
-
