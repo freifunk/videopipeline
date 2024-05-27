b78a22cdcddc75a02dd4e9a687d8269f4a5dbe7e
Vijay Reddy <tamalampudivijayakumar@gmail.com>
2024-05-27 22:37:33 +0530

    updated file uploading script

--- scripts/upload_files_to_cdn.sh
+++ scripts/upload_files_to_cdn.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-rsync -4 -a --rsync-path="mkdir -p ${CDN_FILES_FOLDER}/${ACRONYM}/${VIDEO_ID}/ && rsync" *.jpg *.vtt ${CDN_ADDRESS}:/${CDN_FILES_FOLDER}/${ACRONYM}/${VIDEO_ID}/
+# create the folder structure locally first
+mkdir -p ${ACRONYM}/${VIDEO_ID}/
+
+# Then upload that created structure to cdn
+rsync -4 -a ${ACRONYM} ${CDN_ADDRESS}:/${CDN_FILES_FOLDER}/
+
+# Now upload the files to that location
+rsync -4 -a *.jpg *.vtt ${CDN_ADDRESS}:/${CDN_FILES_FOLDER}/${ACRONYM}/${VIDEO_ID}/
+
 
 
